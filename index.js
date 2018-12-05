289ca5d21209a3e97211b8cdec8c34207c4c2334
Federico Salvi <fede@MacBook-Pro.lan>
2018-12-05 22:09:53 +0100

    updated android build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
 import BackgroundMessage from './App/Services/BackgroundMessage'
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent('cboard', () => App)
+AppRegistry.registerComponent(appName, () => App)
 AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => BackgroundMessage)
