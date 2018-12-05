6a7b1061995544a0f7981274eaf344d3503fd57f
Federico Salvi <fede@MacBook-Pro.lan>
2018-12-05 23:56:32 +0100

    goodbye ios

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
 import BackgroundMessage from './App/Services/BackgroundMessage'
-import {name as appName} from './app.json';
+import { name as appName } from './app.json'
 
 AppRegistry.registerComponent(appName, () => App)
 AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => BackgroundMessage)
